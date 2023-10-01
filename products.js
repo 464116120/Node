acacc17c3f9e549967e925ed08b4155547195f86
Ammar <90888159+464116120@users.noreply.github.com>
2023-10-01 17:21:48 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,333 @@
+const products = [
+  {
+    id: 1,
+    productName: "T-shirt sports",
+    description:
+      "The adidas Bayern Munich Home Shirt 2023 2024 has been made for the fans. Designed to replicate what the pros will be wearing on the pitch, the shirt has been crafted with AEROREADY technology to keep you comfortable and dry. ",
+    price: 170,
+    Rating: 5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+      Colors:[
+        "red"
+        ],
+    imageLink: [
+      "https://res.cloudinary.com/dsqjymyvr/image/upload/v1686449236/enmrmzzb25jvfzltihay.webp",
+      "https://res.cloudinary.com/dsqjymyvr/image/upload/v1690827737/jjpigihobpzvn64ml8qg.jpg",
+      "https://res.cloudinary.com/dsqjymyvr/image/upload/v1690829515/lxvrpkwgod0cxyb6qw1t.avif",
+      "https://res.cloudinary.com/dsqjymyvr/image/upload/v1690829473/fgmzfzcomz4z3nrlqnmr.avif",
+    ],
+  },
+  {
+    id: 2,
+    productName: "T-shirt sports",
+    description:
+      "The adidas Real Madrid Authentic Home Shirt 2023 2024 has been expertly engineered with HEAR.RDY technology to keep you cool by providing excellent air flow as you emulate your favourite players.",
+    price: 180,
+    Rating: 5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+      Colors:[
+        "white"
+        ],
+    imageLink: [
+      "https://res.cloudinary.com/dsqjymyvr/image/upload/v1686448968/v79lrmszmudtidgub89i.webp",
+      "https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831085/dhhb5pjgh818lujmtby1.avif",
+
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831087/wpc5m9fomb8washgcp6d.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831086/ptnvbcyin9zwj9lhircm.avif"
+    ],
+  },
+  {
+    id: 3,
+    productName: "T-shirt sports",
+    description:
+      "The Nike Barcelona Home Shirt 2023 2024 has been crafted in a regular fit and styling using Nike's moisture-wicking Dri-FIT technology to keep you cool and comfortable",
+    price: 120,
+    Rating: 5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+      Colors:[
+        "red"
+        ],
+    imageLink: [
+    "https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831088/kdhmyxhrxzjhwzhf6i6t.avif",
+    "https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831085/j8jvtkrefsbimknzzgeq.avif",
+    "https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831088/xxgv8ywdbn3em7xhfoui.avif",
+    "https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831088/e7dxu1xedys9czdskvdl.avif"
+    ],
+  },
+  {
+    id: 4,
+    productName: "T-shirt sports",
+    description:
+      "The Nike Liverpool Home Shirt 2023 2024 has been engineered with Dri-FIT technology",
+    price: 100,
+    Rating: 4.5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+      Colors:[
+        "red"
+        ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831628/c0uot0fqmdgdk60ij0mt.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831628/fixtwmq2siacph88ivl2.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831629/b8btfdxde8klltzf76nh.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690831627/mmfs1tqqilke0u5evtgh.avif"
+    ],
+  },
+    {
+    id: 5,
+    productName: "T-shirt sports",
+    description:
+      "The Nike Psg Home Shirt 2023 2024 has been engineered with Dri-FIT technology",
+    price: 90,
+    Rating: 4,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+      Colors:[
+        "blue"
+        ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690843011/rybmtfksyqirurxbtey4.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690842969/ot5dohhqgrzx6imdj6x5.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690842969/sqpqf9hztsuw7unlg0fq.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690842967/lqpeefkf0mokfn2p2x08.avif"
+    ],
+  },
+    {
+    id: 6,
+    productName: "T-shirt sports",
+    description:
+      "The adidas Manchester United Authentic Home Shirt 2023 2024 is exactly what the professional Red Devils will be wearing on the pitch. Featuring striped ribbed trims for comfort as you move",
+    price: 110,
+    Rating: 4,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+      Colors:[
+        "red"
+        ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690843624/kc1mvy6e6i493ilwvwil.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690843623/l3imdqsr0t9vznjqksn2.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690843624/nfz9dtxkpnxlscalerd8.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690843623/rfjvmiqxzg2xgjm6gxn7.avif"
+    ],
+  },
+      {
+    id: 7,
+    productName: "Shoes sports",
+    description:
+      "The Nike Court Vision Mens Trainers are a great addition to your off duty wardrobe, crafted with a low profile ankle collar coupled with a lace fastening and a cushioned insole for a secure and comfortable fit.",
+    price: 62,
+    Rating: 5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    
+      ],
+      Colors:[
+        "black"
+        ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858370/ekpbaj7gebbizlnxrtwv.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858368/zo3mpyiw2ofxiyxeqvqu.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858365/ff65mnv6eh5q78otigwo.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858366/qrdbpcdnf4ulmxe5ertv.avif"
+    ],
+  },
+
+        {
+    id: 8,
+    productName: "Shoes sports",
+    description:
+      "The Nike Court Borough Trainers are ideal for casual, everyday wear. The trainers combine a durable upper construction with chunky midsole that ensures support under the foot. The trainers feature Nike swoosh branding down the side of the trainers for a great design",
+    price: 43,
+    Rating: 4,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    
+      ],
+      Colors:[
+        "white"
+        ],
+    imageLink: [
+
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858367/ehzj4hxrnvimlur39vn3.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858370/avheij2ozsvqtbvv4fwg.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858370/avheij2ozsvqtbvv4fwg.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858368/uab5vnxh4h7xawqloequ.avif"
+    ],
+  },
+          {
+    id: 9,
+    productName: "Shoes sports",
+    description:
+      "The Nike Court Borough Trainers are ideal for casual, everyday wear. The trainers combine a durable upper construction with chunky midsole that ensures support under the foot. The trainers feature Nike swoosh branding down the side of the trainers for a great design",
+    price: 42,
+    Rating: 4.5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    
+      ],
+      Colors:[
+        "red"
+        ],
+    imageLink: [
+
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858370/wclg29punpbgciythf8c.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858368/zc14fs8ir2oc6r2oaia8.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858362/gggebns1edjuxcitdas2.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690858366/hzkdijbjrcsccljykrvm.avif"
+    ],
+  },
+            {
+    id: 10,
+    productName: "Shoes sports",
+    description:
+      "Style and comfort combine to make these Nike Court Borough Mid Top Junior Trainers which benefit from having a cushioned insole and padded ankle, whilst the textured sole is assisted by two pivot points to ensure great traction and mobility and the Nike Swoosh branding completes the iconic design.",
+    price: 47,
+    Rating:5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    
+      ],
+      Colors:[
+        "white"
+        ],
+    imageLink: [
+
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690859229/ozokvhgn2v9myskuvk2s.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690859228/lxh2b9u64ldf1o3cvwpq.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690859229/pmu1r3apurjaeovpthqe.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690859229/jtg8jo64arubvx0di5fu.avif"
+    ],
+  },
+    {
+    id: 11,
+    productName: "T-shirt",
+    description:
+      "Have your staples up to date, keeping your casual fits stylish and classic. This 3 Pack of basic T Shirts from DKNY have been designed in 3 different colour ways, ",
+    price: 30,
+    Rating: 4.5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960909/sshrmu8pygnm5xinvvkj.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960909/sztt6a1isfksho40as2u.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960907/czpeo0h2crvostiumigd.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960908/a3t3p1rkf0fvwcgp37gs.avif"
+
+    ],
+  },
+    {
+    id: 12,
+    productName: "T-shirt",
+    description:
+      "T-shirt  basic",
+    price: 15,
+    Rating: 4,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960910/mve57tjfc5imllvojtzh.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960907/czpeo0h2crvostiumigd.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960905/podfwrrvkys9z0oet9s7.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960909/sztt6a1isfksho40as2u.avif"
+    ],
+  },
+    {
+    id: 13,
+    productName: "T-shirt",
+    description:
+      "The Mens Under Armour ABC Camo Short Sleeve T Shirt is a great addition to your weekend wardrobe, featuring a lightweight fabrication coupled with a crew neck collar and short sleeves that offers all day comfort. ",
+    price: 40,
+    Rating: 5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960909/c8lhzggjf6oxcq6nyibo.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960910/yoy61ifley5em4ehehwl.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960911/xqtp5wkkhxb7sr6r7lee.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690960910/zauukearcfdp5k0dyssk.avif"
+    ],
+  },
+  {
+  id: 14,
+    productName: "T-shirt",
+    description:
+      "adidas Essentials Embroidered Linear Logo T-Shirt maleSometimes you just have to keep things simple. No fuss. Easy. Enter this adidas t-shirt. ",
+    price: 16,
+    Rating: 5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690961902/uwse2ouita8eukfiz7em.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690961903/ufs2gio0crjisklex3og.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690961902/xhsjgtshpx2bguk41hw7.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1690961903/wyybwk8x0xcpdtimaq18.avif"
+    ],
+  },
+];
+module.exports = products;
