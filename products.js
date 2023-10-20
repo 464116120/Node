9072404549ff085344e1c4983c47012226f6bd3a
Ammar <90888159+464116120@users.noreply.github.com>
2023-10-20 11:38:39 +0300

    Add files via upload
    Update file

--- products.js
+++ products.js
@@ -437,6 +437,88 @@ const products = [
 "https://res.cloudinary.com/dsqjymyvr/image/upload/v1696812667/g6m9ss4ts9wtyrmgpife.avif",
 "https://res.cloudinary.com/dsqjymyvr/image/upload/v1696812666/oxrzc8phxaealrip5hpq.avif"
     ],
+  },
+              {
+    id: 19,
+    productName: "Short",
+    isTshirt: false,
+    description: "The Womens Nike 10K Dry Shorts are perfect for the hitting the gym or the trails, crafted with an elasticated waistband with an adjustable inner drawcord along with flat lock seams for a comfortable fit. These shorts also boast Dri-Fit technology",
+    price: 12,
+    Rating: 4.5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697788475/45701903_l_a1_ywvqgq.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697788475/45701903_l_f202vk.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697788475/45701902_l_yyyisc.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697788476/45701902_l_a1_wmelsy.avif"
+    ],
   },  
+              {
+    id: 20,
+    productName: "Short",
+    isTshirt: false,
+    description: "Stay cool when training in these Under Armour Fly By 2 Shorts. Cut to a loose fit and constructed from a lightweight poly for a breathable feel",
+    price: 22,
+    Rating: 5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+    imageLink: [
+
+    ],
+  },
+                {
+    id: 21,
+    productName: "Short",
+    isTshirt: false,
+    description: "Enhance your pool-side attire with the Nike Core Swim Shorts. Crafted in a lightweight polyester material with mesh inner briefs, these swim shorts are cut in a regularl",
+    price: 23,
+    Rating: 5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697789302/35216421_l_khfm3b.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697789302/35216421_l_a1_xzo96r.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697789302/35216403_l_b9e5tb.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697789302/35216403_l_a1_atpira.avif"
+    ],
+  },
+                  {
+    id: 22,
+    productName: "Short",
+    isTshirt: false,
+    description: "The stunning adidas Sereno 14 Shorts Mens are made with ClimaLite mesh fabric that will wick sweat from the skin, helping you to keep dry and comfortable during your matches or training sessions.",
+    price: 18,
+    Rating: 5,
+    Size: [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "2XL"
+      ],
+    imageLink: [
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697789302/35216421_l_khfm3b.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697790328/46303522_l_yhedcw.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697790329/46303501_l_i59wxd.avif",
+"https://res.cloudinary.com/dsqjymyvr/image/upload/v1697790329/46303561_l_kcj2vs.avif"
+    ],
+  },
+  
 ];
 module.exports = products;
